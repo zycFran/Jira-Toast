3d6828b4b6fdc014a9e2d4f9ac0946943340da4b
zyc302546 <525151673@qq.com>
2015-10-10 17:17:02 +0800

    Jira issue 自动提醒功能 0.0.1版

---
+++ js/util.js
@@ -0,0 +1,27 @@
+/**
+ * Created by zhouyc on 2015/10/10.
+ */
+function getAjax(url, cb){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            //msg_cache = xhr.responseText;
+            cb(xhr.responseText);
+        }
+    };
+    xhr.send();
+}
+
+function postAjax(url, data, cb){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type" , "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            //msg_cache = xhr.responseText;
+            cb(xhr.responseText);
+        }
+    };
+    xhr.send(data);
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+//var http = require('http');
+
+//socket.io setup
+var io = require('socket.io').listen(3001);
+
+io.sockets.on('connection', function(socket) {
+  console.log('socket.io client connected');
+  socket.on('disconnect', function() {
+    console.log('socket.io client disconnected');
+  });
+});
+
+console.log('socket.io server on port 3001');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+router.post('/jira/webhook', function (req, res) {
+  io.sockets.emit('ping',req.body);
+});
+app.use("/", router);
+
+
+app.listen(3002, function(){
+  console.log("app server on port 3002");
+});
